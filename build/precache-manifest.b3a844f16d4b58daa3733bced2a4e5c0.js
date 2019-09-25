862f5363bdab8241d18a1a12d28816aaa809e3dc
mikailbayram <mikailbosna98@gmail.com>
2019-09-25 15:59:52 +0200

    build folders

---
+++ build/precache-manifest.b3a844f16d4b58daa3733bced2a4e5c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b84e1dd6757e8d78b6c39672d5f1bed0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "356fb016306da3aae2c0",
+    "url": "/static/css/2.60f48ce4.chunk.css"
+  },
+  {
+    "revision": "2c22a6603599876706b2",
+    "url": "/static/css/main.0502d12d.chunk.css"
+  },
+  {
+    "revision": "356fb016306da3aae2c0",
+    "url": "/static/js/2.2bf39ea7.chunk.js"
+  },
+  {
+    "revision": "2c22a6603599876706b2",
+    "url": "/static/js/main.5f84e1b4.chunk.js"
+  },
+  {
+    "revision": "ea42e00bd0fdda4fcb8c",
+    "url": "/static/js/runtime-main.c8e33a0c.js"
+  }
+]);
